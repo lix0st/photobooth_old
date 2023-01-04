0d690cffd12e3979513245831f1856d56717d73d
lix0st <olivierroth2@gmail.com>
2023-01-04 20:33:06 +0100

    add custom config to repository

---
+++ config/my.config.inc.php
@@ -0,0 +1,31 @@
+<?php
+$config = array (
+  'show_fork' => false,
+  'previewCamFlipHorizontal' => false,
+  'camera_mode' => 'environment',
+  'cntdwn_time' => '3',
+  'cheese_time' => '1',
+  'use_collage' => true,
+  'background_image' => 'url(../img/bg_aim.jpg)',
+  'background_admin' => 'url(../img/bg_aim.jpg)',
+  'background_chroma' => 'url(../img/bg.jpg)',
+  'language' => 'fr',
+  'start_screen_title' => false,
+  'start_screen_subtitle' => 'Photomaton',
+  'is_event' => true,
+  'event' => 
+  array (
+    'textLeft' => 'Alexandra',
+    'textRight' => 'Olivier',
+    'symbol' => 'fa-heart',
+  ),
+  'font_size' => '30px',
+  'colors' => 
+  array (
+    'primary' => '#87cbb3',
+    'secondary' => '#26a269',
+    'button_font' => '#000000',
+  ),
+  'scrollbar' => true,
+  'reset_remove_config' => false,
+);
